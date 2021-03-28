676ce3b9b4583813bcca12a9721b0bdadbb75123
Maro de Melo <marodemelo10@gmail.com>
2021-03-27 21:17:50 -0300

    Created field tile style: opened, nearMines and mined

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import params from './src/params'
+import Field from './src/components/Field'
 
 
 
@@ -10,6 +11,15 @@ export default class App extends Component{
       <View style={styles.container}>
         <Text style={styles.welcome}> Campo minado</Text>
         <Text style={styles.instructions}> Tamanho da grade: {params.getRowsAmount()} x {params.getColumnsAmount()}</Text>
+        <Field/>
+        <Field opened/>
+        <Field opened nearMines={1}/>
+        <Field/>
+        <Field opened nearMines={2}/>
+        <Field/>
+        <Field opened nearMines={5}/>
+        <Field opened nearMines={7}/>
+        <Field/>
       </View>
     )
   }
