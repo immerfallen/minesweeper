6141ea7faa0b8fdfacb29b07f65f3564259a53ce
Maro de Melo <marodemelo10@gmail.com>
2021-03-28 14:31:57 -0300

    created component mine, flag and field

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import params from './src/params'
 import Field from './src/components/Field'
+import Flag from './src/components/Flag'
 
 
 
@@ -11,7 +12,7 @@ export default class App extends Component{
       <View style={styles.container}>
         <Text style={styles.welcome}> Campo minado</Text>
         <Text style={styles.instructions}> Tamanho da grade: {params.getRowsAmount()} x {params.getColumnsAmount()}</Text>
-        <Field/>
+        <Field flagged opened/>
         <Field opened/>
         <Field opened nearMines={1}/>
         <Field/>
@@ -19,7 +20,11 @@ export default class App extends Component{
         <Field/>
         <Field opened nearMines={5}/>
         <Field opened nearMines={7}/>
-        <Field/>
+        <Field flagged/>
+        <Field mined/>
+        <Field mined opened/>
+        <Field mined opened exploded/>
+        
       </View>
     )
   }
