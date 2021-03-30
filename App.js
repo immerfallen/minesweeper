10bfaded04cff53e049a4ea199a1bad97af3a399
Maro de Melo <marodemelo10@gmail.com>
2021-03-30 20:23:57 -0300

    created components minedField and rendered minedField in the View

--- App.js
+++ App.js
@@ -1,29 +1,41 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
 import params from './src/params'
-import Field from './src/components/Field'
-
-
-
+import MineField from './src/components/MineField'
+import {createMinedBoard} from './src/functions'
 
 export default class App extends Component{
+
+  constructor(props){
+    super(props)
+    this.state = this.createState()
+  }
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    
+    return Math.ceil(cols*rows*params.difficultLevel)
+
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: createMinedBoard(rows,cols, this.minesAmount())
+
+    }
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}> Campo minado</Text>
-        <Text style={styles.instructions}> Tamanho da grade: {params.getRowsAmount()} x {params.getColumnsAmount()}</Text>
-        <Field flagged opened/>
-        <Field opened/>
-        <Field opened nearMines={1}/>
-        <Field/>
-        <Field opened nearMines={2}/>
-        <Field/>
-        <Field opened nearMines={5}/>
-        <Field opened nearMines={7}/>
-        <Field flagged/>
-        <Field mined/>
-        <Field mined opened/>
-        <Field mined opened exploded/>
+        <Text> Campo minado</Text>
+        <Text> Tamanho da grade: {params.getRowsAmount()} x {params.getColumnsAmount()}</Text>          
+        <View style={styles.board}>
+          <MineField board={this.state.board}/>
+        </View>
         
       </View>
     )
@@ -33,13 +45,10 @@ export default class App extends Component{
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff'
+    justifyContent: 'flex-end',    
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#aaa',
   }
 })
--- src/functions.js
+++ src/functions.js
@@ -1,12 +1,12 @@
 const createBoard = (rows, columns) => {
-    return Array(rows).fill(0).map((_,row) =>{
-        return Array(columns).fill(0).fill((_, column)=>{
+    return Array(rows).fill(0).map((_, row) => {
+        return Array(columns).fill(0).map((_, column) => {
             return {
-                row: row,
-                column: column,
+                row,
+                column,
                 opened: false,
                 flagged: false,
-                mined:false,
+                mined: false,
                 exploded: false,
                 nearMines: 0
             }
@@ -18,23 +18,26 @@ const spreadMines = (board, minesAmount) => {
     const rows = board.length
     const columns = board[0].length
     let minesPlanted = 0
+    
+    while (minesPlanted < minesAmount) {
+        const rowSel = parseInt(Math.random() * rows, 10)      
+        const columnSel = parseInt(Math.random() * columns, 10)                    
 
-    while(minesPlanted < minesAmount){
-        const rowSel = parseInt(Math.random()*rows,10)
-        const columnSel = parseInt(Math.random()*column,10)
-
-        if(!board[rowSel[columnSel]].mined){
+        if (!board[rowSel][columnSel].mined) {
             board[rowSel][columnSel].mined = true
             minesPlanted++
+           
         }
-
     }
 }
 
-const createMineBoard = ( rows,columns, minesAmount) => {
-    const board = createBoard(rows,columns) 
+const createMinedBoard = (rows, columns, minesAmount) => {
+    const board = createBoard(rows, columns)
     spreadMines(board, minesAmount)
     return board
 }
 
-export {createMineBoard}
+
+
+
+export {createMinedBoard}
